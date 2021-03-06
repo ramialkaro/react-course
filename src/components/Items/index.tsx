c55fe840d9e87a1922204d5844d92dff23938a54
ramialkaro <rami.alkaro@gmail.com>
2021-03-06 14:48:36 +0200

    use the ItemsContainer component instead of dummt text

--- src/components/Items/index.tsx
+++ src/components/Items/index.tsx
@@ -1,10 +1,8 @@
 import React from 'react'
-
+import ItemsContainer from "./ItemsContainer";
 const Items = () => {
     return (
-        <div>
-            my items
-        </div>
+        <ItemsContainer/>
     )
 }
 
