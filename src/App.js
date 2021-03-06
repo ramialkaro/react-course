be737a0b00efaa6ff03a47f7ff977c99839de2c6
ramialkaro <rami.alkaro@gmail.com>
2021-03-06 12:27:26 +0200

    center the form in the center of the page

--- src/App.js
+++ src/App.js
@@ -4,23 +4,23 @@ import { Grid } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
-    root: {},
+  root: {
+    height: "100vh",
+    display: "flex",
+    alignItems: "center",
+    justify: "center",
+  },
 }));
 
 function App() {
-    const classes = useStyles();
-    return ( <
-        div className = "App" >
-        <
-        header className = "App-header" > < /header> <
-        Grid container justify = "center"
-        alignItems = "center" >
-        <
-        Login / >
-        <
-        /Grid> < /
-        div >
-    );
+  const classes = useStyles();
+  return (
+    <Grid className={classes.root}>
+      <Grid container justify="center" alignItems="center">
+        <Login />
+      </Grid>{" "}
+    </Grid>
+  );
 }
 
 export default App;
